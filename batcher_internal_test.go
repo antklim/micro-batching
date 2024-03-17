65f05786988147bdbfca1ebf6b3bebe565a2cbf9
antklim <antklim@gmail.com>
2024-03-17 21:40:20 +1100

    test: added batcher test

---
+++ batcher_internal_test.go
@@ -0,0 +1,35 @@
+package microbatching
+
+import (
+	"testing"
+
+	"github.com/oklog/ulid/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBatch(t *testing.T) {
+	jobs := make(chan job, 100)
+	defer close(jobs)
+
+	bp := &TestBP{}
+
+	b := batcher{
+		batchSize: 3,
+		jobs:      jobs,
+		p:         bp,
+	}
+
+	for i := 0; i < 11; i++ {
+		jobs <- job{ID: ulid.Make(), J: &TestJob{ID: i}}
+	}
+
+	// make sure the jobs are in the queue before calling batch
+	assert.Equal(t, 11, len(jobs))
+
+	b.batch()
+
+	assert.Equal(t, 0, len(jobs))
+
+	// batch processor should have been called 4 times (3 + 3 + 3 + 2)
+	assert.Equal(t, uint32(4), bp.Counter())
+}
